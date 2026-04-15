9bc6a13317f2097d2f079d4ff2971951b1c7b884
Seu Nome <seu@email.com>
2026-04-15 17:22:12 -0300

    ci: configurar deploy no GitHub Pages com secrets

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/CPE_P1/' : '/',
   plugins: [react()],
   assetsInclude: ['**/*.tsv'],
 })
